3b77b7be7e432d8c8f0c2df2c468f3ee2c45a795
Milind Randive <149869138+Milind0616@users.noreply.github.com>
2025-01-22 19:53:59 +0530

    ok
    done

---
+++ src/components/Feature.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Feature = () => {
+    return (
+        <section id='feature' className='section-p1'>
+            <div className='feature__container'>
+                <img src="img/features/f1.png" alt="" />
+                <h5>Free Shipping</h5>
+            </div>
+            <div className='feature__container'>
+                <img src="img/features/f2.png" alt="" />
+                <h5>Online order</h5>
+            </div>
+            <div className='feature__container'>
+                <img src="img/features/f3.png" alt="" />
+                <h5>Save Money</h5>
+            </div>
+            <div className='feature__container'>
+                <img src="img/features/f4.png" alt="" />
+                <h5>Promotions</h5>
+            </div>
+            <div className='feature__container'>
+                <img src="img/features/f5.png" alt="" />
+                <h5>Happy Self</h5>
+            </div>
+            <div className='feature__container'>
+                <img src="img/features/f6.png" alt="" />
+                <h5>24/7 Support</h5>
+            </div>
+        </section>
+    )
+}
+
+export default Feature
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+        <h4>Trade-in-offer</h4>
+        <h2>Super value dealse</h2>
+        <h1>On all product</h1>
+        <p>Save more with coupons and up to 70% off!</p>
+        <button>Shop Now</button>
+    </section>
+  )
+}
+
+export default Hero;
