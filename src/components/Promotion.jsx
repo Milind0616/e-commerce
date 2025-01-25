5b382d9b32d5eb386aafb9326fe8cf10e6a6068e
Milind Randive <randivemilind1@gamil.com>
2025-01-25 17:26:33 +0530

    getting error in code

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 
 const Promotion = () => {
     return (
-        <section id='promo' className='section-p100'>
-            <div className='promo-box'>
-                <h4>Crazy delas</h4>
+        <section id='sm-banner' className='.section-p1 '>
+            <div className='banner-box'>
+                <h4>crazy deals</h4>
                 <h2>buy 1 get 1 free</h2>
                 <span>The best classic dress is on sale at cara</span>
                 <button>Learn More</button>
             </div>
-            <div className='promo-box2'>
-                <h4>Crazy delas</h4>
+
+            <div className='banner-box'>
+                <h4>crazy deals</h4>
                 <h2>buy 1 get 1 free</h2>
                 <span>The best classic dress is on sale at cara</span>
                 <button>Learn More</button>
             </div>
+
         </section>
     )
 }
